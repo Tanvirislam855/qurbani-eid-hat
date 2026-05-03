79a8055353f5dfd192d9b7d6069a0e9090c31cdf
Tanvirislam855e <ajtanvir855@gmail.com>
2026-05-04 04:25:32 +0600

    sort page edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+
 import Image from "next/image";
 import Banner from "../components/Banner"
 import TopGenerations from "@/components/TopGenerations";
 import Testimonials from "@/components/Testimonials"
+import SortByPrice from "@/components/SortByPrice";
 import 'animate.css';
 
 export default function Home() {
@@ -9,6 +11,9 @@ export default function Home() {
 
     <div>
       <Banner/>
+      <div className="flex justify-end mt-15 text-center px-10">
+      <SortByPrice/>
+      </div>
       <TopGenerations/>
       <Testimonials/>
     </div>
