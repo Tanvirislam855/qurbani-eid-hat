f2e27219345c3c96b75553f7465c265d6d4aae0c
Tanvirislam855e <ajtanvir855@gmail.com>
2026-05-03 23:45:37 +0600

    auth js edit

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { authClient } from "@/lib/auth-client";
 import { Check } from "@gravity-ui/icons";
-import { FcGoogle } from "react-icons/fc"; 
+import { FcGoogle } from "react-icons/fc";
 import {
   Button,
   Card,
@@ -15,71 +15,62 @@ import {
 import { useRouter } from "next/navigation";
 
 export default function SignInPage() {
-
-    const router = useRouter()
+  const router = useRouter();
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    const {data, error} = await authClient.signUp.email({
-        
-        email,
-        password,
-        callbackURL:'/'
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+      callbackURL: "/",
     });
-    
 
-    console.log({data, error});
+    console.log({ data, error });
 
-    
-
-
-    // if(!error) {
-    //     router.push('/')
-    // }
-
+    if (!error) {
+      router.push("/");
+    }
   };
+
   const handleGoogleSignIn = async () => {
     await authClient.signIn.social({
-        provider: 'google'
-         })
-  }
+      provider: "google",
+      callbackURL: "/",
+    });
+  };
 
   return (
     <Card className="border mx-auto w-125 py-10 mt-5">
       <h1 className="text-center text-2xl font-bold">Sign In</h1>
 
       <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
-        
-
-        
-
         <TextField
           isRequired
           name="email"
           type="email"
           validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)
+            ) {
               return "Please enter a valid email address";
             }
-
             return null;
           }}
         >
           <Label>Email</Label>
-          <Input placeholder="john@example.com" />
+          <Input name="email" placeholder="john@example.com" />
           <FieldError />
         </TextField>
 
         <TextField
           isRequired
-          minLength={8}
           name="password"
           type="password"
+          minLength={8}
           validate={(value) => {
             if (value.length < 8) {
               return "Password must be at least 8 characters";
@@ -90,12 +81,11 @@ export default function SignInPage() {
             if (!/[0-9]/.test(value)) {
               return "Password must contain at least one number";
             }
-
             return null;
           }}
         >
           <Label>Password</Label>
-          <Input placeholder="Enter your password" />
+          <Input name="password" placeholder="Enter your password" />
           <Description>
             Must be at least 8 characters with 1 uppercase and 1 number
           </Description>
@@ -112,9 +102,17 @@ export default function SignInPage() {
           </Button>
         </div>
       </Form>
-      <p className="text-center">or</p>
-      <Button onClick={handleGoogleSignIn} variant="tertiary" className={'w-full'}><FcGoogle className="text-xl" />Sign In With Google</Button>
 
+      <p className="text-center my-3">or</p>
+
+      <Button
+        onClick={handleGoogleSignIn}
+        variant="tertiary"
+        className="w-full"
+      >
+        <FcGoogle className="text-xl" />
+        Sign In With Google
+      </Button>
     </Card>
   );
 }
--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
 
-    baseURL: "http://qurbani-eid-hat.vercel.app"
+    baseURL: "http://localhost:3000"
 })
 export const { signIn, signup, signOut, useSession } = createAuthClient ()
